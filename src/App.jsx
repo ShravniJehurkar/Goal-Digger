19b414844b976c33ab23cce62aacb4818f86e441
Shravni <143245717+ShravniJehurkar@users.noreply.github.com>
2025-04-02 21:13:22 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Questionnaire from './pages/Questionnaire';
+import UserProfile from './pages/UserProfile';
+import IkigaiQuestionnaire from './pages/IkigaiQuestionnaire';
+import FunQuestionnaire from './pages/FunQuestionnaire';
 import Results from './pages/Results';
 import About from './pages/About';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/questionnaire" element={<Questionnaire />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<UserProfile />} />
+              <Route path="/ikigai-questionnaire" element={<IkigaiQuestionnaire />} />
+              <Route path="/fun-questionnaire" element={<FunQuestionnaire />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
